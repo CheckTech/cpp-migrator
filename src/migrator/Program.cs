5f4ee4d813a0ab0de1d958d487f71941c64f556d
Brian Gillespie <brian@mcneel.com>
2016-10-28 12:10:27 -0700

    Add RhinoSdkPath macro to migrated plug-ins
    Fixes RH-36088

--- src/migrator/Program.cs
+++ src/migrator/Program.cs
@@ -13,7 +13,7 @@ namespace migrator
 {
   class Program
   {
-    static bool rename_build_configurations = true;
+    static bool _renameBuildConfigurations = true;
 
     static void Main(string[] args)
     {
@@ -30,7 +30,7 @@ namespace migrator
       MakeCodeChanges(projectFolder);
     }
 
-    static void MakeSolutionChanges(string projectFolder)
+    private static void MakeSolutionChanges(string projectFolder)
     {
       foreach (var file in Directory.GetFiles(projectFolder, "*.sln"))
       {
@@ -48,130 +48,148 @@ namespace migrator
           }
         }
 
-        System.IO.File.WriteAllText(file, body.ToString());
+        File.WriteAllText(file, body.ToString());
       }
     }
 
     static void MakeProjectChanges(string projectFolder)
     {
       // Update .vcxproj files
-      foreach (var file in System.IO.Directory.GetFiles(projectFolder, "*.vcxproj"))
+      string ns = "{http://schemas.microsoft.com/developer/msbuild/2003}";
+      foreach (var file in Directory.GetFiles(projectFolder, "*.vcxproj"))
       {
         XDocument vcxproj = XDocument.Load(file);
-        string ns = "{http://schemas.microsoft.com/developer/msbuild/2003}";
 
         // Update tools version
-        vcxproj.Root.SetAttributeValue("ToolsVersion", "14.0");
-
-        // Update Project Configurations
-
-        // First, see if the DebugRhino already exists, and don't do this twice.
-        var existing_configuration = vcxproj.Root.Descendants(ns + "ProjectConfiguration").FirstOrDefault(el => el.Attribute("Include")?.Value == "DebugRhino|x64");
-        if (existing_configuration != null)
-          rename_build_configurations = false;
-
-        var elements_to_remove = new List<XElement>();
-        foreach (XElement project_configuration in vcxproj.Root.Elements(ns + "ItemGroup").Elements(ns + "ProjectConfiguration"))
+        if (vcxproj.Root != null)
         {
-          var include = project_configuration.Attribute("Include").Value;
-          if (include.Contains("Win32"))
+          vcxproj.Root.SetAttributeValue("ToolsVersion", "14.0");
+
+          // Update Project Configurations
+
+          // First, see if the DebugRhino already exists, and don't do this twice.
+          var existingConfiguration = vcxproj.Root.Descendants(ns + "ProjectConfiguration").FirstOrDefault(el => el.Attribute("Include")?.Value == "DebugRhino|x64");
+          if (existingConfiguration != null)
+            _renameBuildConfigurations = false;
+
+          var elementsToRemove = new List<XElement>();
+          foreach (XElement projectConfiguration in vcxproj.Root.Elements(ns + "ItemGroup").Elements(ns + "ProjectConfiguration"))
           {
-            // Remove Win32 Project Configurations
-            elements_to_remove.Add(project_configuration);
-          }
-
-          if (rename_build_configurations && include.StartsWith("Debug|x64"))
-          {
-            // Rename Debug project configurations to DebugRhino
-            var config = project_configuration.Element(ns + "Configuration");
-            project_configuration.SetAttributeValue("Include", "DebugRhino|x64");
-            config.Value = "DebugRhino";
-          }
-
-          if (include.StartsWith("PseudoDebug|x64"))
-          {
-            // Rename PseudoDebug project configurations to Debug
-            var config = project_configuration.Element(ns + "Configuration");
-            project_configuration.SetAttributeValue("Include", "Debug|x64");
-            config.Value = "Debug";
-          }
-
-
-        }
-
-
-        // Update Build Settings
-        foreach (XElement property_group in vcxproj.Root.Elements(ns + "PropertyGroup"))
-        {
-          if (CleanupElementCondition(property_group))
-            elements_to_remove.Add(property_group);
-
-          var propertyGroupLabel = property_group.Attribute("Label")?.Value;
-          var platformToolset = property_group.Element(ns + "PlatformToolset");
-          if (platformToolset == null && propertyGroupLabel != null && propertyGroupLabel == "Configuration")
-          {
-            platformToolset = new XElement(ns + "PlatformToolset");
-            property_group.Add(platformToolset);
-          }
-
-          if (platformToolset != null)
-            platformToolset.Value = "v140";
-        }
-
-        foreach (XElement item_definition_group in vcxproj.Root.Elements(ns + "ItemDefinitionGroup"))
-        {
-          if (CleanupElementCondition(item_definition_group))
-            elements_to_remove.Add(item_definition_group);
-        }
-
-        // Precompiled Header settings
-        foreach (XElement item_definition_group in vcxproj.Root.Elements(ns + "ItemGroup").Elements(ns + "ClCompile"))
-        {
-          var include_attribute = item_definition_group.Attribute("Include");
-          if (include_attribute == null)
-            continue;
-
-          var include = include_attribute.Value;
-
-          if (include == "stdafx.cpp")
-          {
-            var precompiled_header_elements = item_definition_group.Elements(ns + "PrecompiledHeader");
-            foreach (var element in precompiled_header_elements)
+            var xAttribute = projectConfiguration.Attribute("Include");
+            if (xAttribute != null)
             {
-              if (CleanupElementCondition(element))
-                elements_to_remove.Add(element);
+              var include = xAttribute.Value;
+              if (include.Contains("Win32"))
+              {
+                // Remove Win32 Project Configurations
+                elementsToRemove.Add(projectConfiguration);
+              }
+
+              if (_renameBuildConfigurations && include.StartsWith("Debug|x64"))
+              {
+                // Rename Debug project configurations to DebugRhino
+                var config = projectConfiguration.Element(ns + "Configuration");
+                projectConfiguration.SetAttributeValue("Include", "DebugRhino|x64");
+                if (config != null) config.Value = "DebugRhino";
+              }
+
+              if (include.StartsWith("PseudoDebug|x64"))
+              {
+                // Rename PseudoDebug project configurations to Debug
+                var config = projectConfiguration.Element(ns + "Configuration");
+                projectConfiguration.SetAttributeValue("Include", "Debug|x64");
+                if (config != null) config.Value = "Debug";
+              }
             }
           }
-        }
 
-        // Add targetver.h file 
-        var result = vcxproj.Root.Descendants(ns + "ClInclude").FirstOrDefault(el => el.Attribute("Include")?.Value == "targetver.h");
-        if (result == null)
-        {
-          result = vcxproj.Root.Descendants(ns + "ClInclude").FirstOrDefault(el => el.Attribute("Include")?.Value == "stdafx.h");
-          result.Parent.Add(new XElement(ns + "ClInclude", new XAttribute("Include", "targetver.h")));
-        }
 
-        // Add property sheets
-        foreach (var property_sheet_element in vcxproj.Root.Descendants(ns + "ImportGroup").Where(el => el.Attribute("Label")?.Value == "PropertySheets"))
-        {
-          if (CleanupElementCondition(property_sheet_element))
+          // Update Build Settings
+          foreach (XElement propertyGroup in vcxproj.Root.Elements(ns + "PropertyGroup"))
           {
-            elements_to_remove.Add(property_sheet_element);
-            continue;
+            if (CleanupElementCondition(propertyGroup))
+              elementsToRemove.Add(propertyGroup);
+
+            var propertyGroupLabel = propertyGroup.Attribute("Label")?.Value;
+            var platformToolset = propertyGroup.Element(ns + "PlatformToolset");
+            if (platformToolset == null && propertyGroupLabel != null && propertyGroupLabel == "Configuration")
+            {
+              platformToolset = new XElement(ns + "PlatformToolset");
+              propertyGroup.Add(platformToolset);
+            }
+
+            if (platformToolset != null)
+              platformToolset.Value = "v140";
           }
-          result = property_sheet_element.Descendants("Import").FirstOrDefault(el => ((string)el.Attribute("Project")?.Value).EndsWith("Rhino.Cpp.PlugIn.props"));
-          if (result == null) // No Rhino Property Sheets here
+
+          foreach (XElement itemDefinitionGroup in vcxproj.Root.Elements(ns + "ItemDefinitionGroup"))
           {
-            property_sheet_element.Add(new XElement(ns + "Import", new XAttribute("Project", "$(ProgramW6432)\\Rhino 6.0 SDK\\PropertySheets\\Rhino.Cpp.PlugIn.props")));
+            if (CleanupElementCondition(itemDefinitionGroup))
+              elementsToRemove.Add(itemDefinitionGroup);
           }
+
+          // Precompiled Header settings
+          foreach (XElement itemDefinitionGroup in vcxproj.Root.Elements(ns + "ItemGroup").Elements(ns + "ClCompile"))
+          {
+            var includeAttribute = itemDefinitionGroup.Attribute("Include");
+            if (includeAttribute == null)
+              continue;
+
+            var include = includeAttribute.Value;
+
+            if (include == "stdafx.cpp")
+            {
+              var precompiledHeaderElements = itemDefinitionGroup.Elements(ns + "PrecompiledHeader");
+              foreach (var element in precompiledHeaderElements)
+              {
+                if (CleanupElementCondition(element))
+                  elementsToRemove.Add(element);
+              }
+            }
+          }
+
+          // Add targetver.h file 
+          var result = vcxproj.Root.Descendants(ns + "ClInclude").FirstOrDefault(el => el.Attribute("Include")?.Value == "targetver.h");
+          if (result == null)
+          {
+            result = vcxproj.Root.Descendants(ns + "ClInclude").FirstOrDefault(el => el.Attribute("Include")?.Value == "stdafx.h");
+            result?.Parent?.Add(new XElement(ns + "ClInclude", new XAttribute("Include", "targetver.h")));
+          }
+
+          // Add Macro to find SDK
+          // Does it exist?
+          var existingSdkPathMacro = vcxproj.Root.Descendants(ns + "RhinoSdkPath");
+          if (!existingSdkPathMacro.Any())
+          {
+            // Nope. Add the macro.
+            var macroElement = new XElement(ns + "PropertyGroup");
+            var sdkVersionElement = new XElement(ns + "RhinoSdkVersion") {Value = "6.0"};
+            var sdkPathElement = new XElement(ns + "RhinoSdkPath");
+            sdkPathElement.Value = @"$([MSBuild]::GetRegistryValueFromView('HKEY_LOCAL_MACHINE\SOFTWARE\McNeel\Rhinoceros\SDK\$(RhinoSdkVersion)', 'InstallPath', null, RegistryView.Registry64))";
+            macroElement.Add(sdkVersionElement);
+            macroElement.Add(sdkPathElement);
+            vcxproj.Root.AddFirst(macroElement);
+
+            // Add property sheets
+            foreach (var propertySheetElement in vcxproj.Root.Descendants(ns + "ImportGroup").Where(el => el.Attribute("Label")?.Value == "PropertySheets"))
+            {
+              if (CleanupElementCondition(propertySheetElement))
+              {
+                elementsToRemove.Add(propertySheetElement);
+                continue;
+              }
+              result = propertySheetElement.Descendants("Import").FirstOrDefault(el => (el.Attribute("Project")?.Value).EndsWith("Rhino.Cpp.PlugIn.props"));
+              if (result == null) // No Rhino Property Sheets here
+              {
+                propertySheetElement.Add(new XElement(ns + "Import", new XAttribute("Project", "$(RhinoSdkPath)PropertySheets\\Rhino.Cpp.PlugIn.props")));
+              }
+            }
+          }
+
+          // Remove unwanted elements
+          foreach (var element in elementsToRemove)
+            element.Remove();
         }
-
-
-
-        // Remove unwanted elements
-        foreach (var element in elements_to_remove)
-          element.Remove();
 
         vcxproj.Save(file);
         }
@@ -179,11 +197,11 @@ namespace migrator
 
     static bool CleanupElementCondition(XElement element)
     {
-      var condition_attribute = element.Attribute("Condition");
-      if (condition_attribute == null)
+      var conditionAttribute = element.Attribute("Condition");
+      if (conditionAttribute == null)
         return false;
 
-      var condition = condition_attribute.Value;
+      var condition = conditionAttribute.Value;
       if (condition.Contains("'$(Configuration)|$(Platform)'=='Debug|Win32'"))
         return true;
       if (condition.Contains("'$(Configuration)|$(Platform)'=='Release|Win32'"))
@@ -191,49 +209,49 @@ namespace migrator
       if (condition.Contains("'$(Configuration)|$(Platform)'=='PseudoDebug|Win32'"))
         return true;
 
-      if (rename_build_configurations && condition.Contains("'$(Configuration)|$(Platform)'=='Debug|x64'"))
+      if (_renameBuildConfigurations && condition.Contains("'$(Configuration)|$(Platform)'=='Debug|x64'"))
         element.SetAttributeValue("Condition", "'$(Configuration)|$(Platform)'=='DebugRhino|x64'");
-      if (rename_build_configurations && condition.Contains("'$(Configuration)|$(Platform)'=='PseudoDebug|x64'"))
+      if (_renameBuildConfigurations && condition.Contains("'$(Configuration)|$(Platform)'=='PseudoDebug|x64'"))
         element.SetAttributeValue("Condition", "'$(Configuration)|$(Platform)'=='Debug|x64'");
 
       return false; 
    }
 
-    static void MakeCodeChanges(string projectFolder)
+    private static void MakeCodeChanges(string projectFolder)
     {
-      string json = System.IO.File.ReadAllText(Path.Combine(AssemblyDirectory, "replacements.json"));
+      string json = File.ReadAllText(Path.Combine(AssemblyDirectory, "replacements.json"));
       Dictionary<string, string> replacements = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
 
-      foreach (var file in System.IO.Directory.GetFiles(projectFolder, "*.cpp"))
+      foreach (var file in Directory.GetFiles(projectFolder, "*.cpp"))
       {
         ReplaceInFile(file, replacements);
       }
-      foreach (var file in System.IO.Directory.GetFiles(projectFolder, "*.h"))
+      foreach (var file in Directory.GetFiles(projectFolder, "*.h"))
       {
         ReplaceInFile(file, replacements);
       }
 
       // stdafx.h
-      var stdafx_h_path = System.IO.Path.Combine(projectFolder, "stdafx.h");
-      var stdafx_h_replacements = new Dictionary<string, string>()
+      var stdafxHPath = Path.Combine(projectFolder, "stdafx.h");
+      var stdafxHReplacements = new Dictionary<string, string>()
       {
         {"#pragma once", "#pragma once\r\n#define RHINO_V6_READY"}
       };
-      ReplaceInFile(stdafx_h_path, stdafx_h_replacements);
+      ReplaceInFile(stdafxHPath, stdafxHReplacements);
 
       // Create targetver.h if it doesn't exist
-      var targetver_h_path = Path.Combine(projectFolder, "targetver.h");
-      if (!File.Exists(targetver_h_path))
+      var targetverHPath = Path.Combine(projectFolder, "targetver.h");
+      if (!File.Exists(targetverHPath))
       {
-        File.Copy(Path.Combine(AssemblyDirectory, "targetver.h"), targetver_h_path);
+        File.Copy(Path.Combine(AssemblyDirectory, "targetver.h"), targetverHPath);
       }
     }
 
-    static void ReplaceInFile(string filename, Dictionary<string, string> replacements)
+    private static void ReplaceInFile(string filename, Dictionary<string, string> replacements)
     {
       // Read contents of file into string
-      var body = System.IO.File.ReadAllText(filename);
-      var original_body = body;
+      var body = File.ReadAllText(filename);
+      var originalBody = body;
 
       foreach (var key in replacements.Keys)
       {
@@ -241,11 +259,11 @@ namespace migrator
         body = rx.Replace(body, replacements[key]);
       }
 
-      if (body != original_body)
-        System.IO.File.WriteAllText(filename, body);
+      if (body != originalBody)
+        File.WriteAllText(filename, body);
     }
 
-    static void PrintUsage()
+    private static void PrintUsage()
     {
       Console.WriteLine("migrator.exe <path_to_folder>");
     }
